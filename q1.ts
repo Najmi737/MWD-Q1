a357d7e9ea8a989189e17f8f70418bf86c4061d7
Muhammed Arif Najmi <arifnajmi@yahoo.com>
2023-02-24 17:43:24 +0500

    Add files via upload

---
+++ q1.ts
@@ -0,0 +1,35 @@
+// Ex1 downloads COMPLETE
+
+
+// ex2 Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let aaa = "Eric"; 
+console.log(`Hello ${aaa}, would you like to learn some Python today?`);
+
+console.log(typeof aaa);
+
+// Ex3
+let me = "Arif najmi";
+console.log(me.toUpperCase()); // ARIF NAJMI 
+console.log(me.toLowerCase()); // arif najmi 
+
+
+// Ex4
+console.log('Iqbal said, "tu shahin hai"');
+
+// ex5  Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person = "Iqbal";
+let message = famous_person + " said, 'tuu shahin hai'"; 
+console.log(message); // Iqbal said, "tu shahin hai"
+
+// ex6 whitespaces          
+
+const WithWhitespace = "\t\n  John Smith   \t\n";
+console.log(WithWhitespace.trim());
+
+
+
+
+
+ 
